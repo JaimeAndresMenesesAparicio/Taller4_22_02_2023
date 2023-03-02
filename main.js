28560d14f0a0146a9ba553d9ed7ac1a95085dbe7
JaimeAndresMenesesAparicio <jaime.meneses89@gmail.com>
2023-03-01 20:28:35 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-// 1.	Pregunto si quiere el huevo frito.
-// Si me dice que si, lo frio, si me dice que no, lo hago hervido.
-// Una vez cocinado le pregunto si quiere sal en el huevo.
-// Si me dice que no lo sirvo en el Plato. Si me dice que si le hecho sal y después lo sirvo en el plato.
+// 2.	3 usuarios entran a una heladería a comprar un helado, pero hay un problema:
+//  los precios no están al lado de cada estante con su respectivo helado.
+//  “Ellos quieren comprar el helado más caro que puedan con la plata que tienen.
+// Miguel tiene $1.000 
+// Carlos tiene $2.000 
+// Manuel tiene $500 
+// Los precios de helados son los siguientes:
+// Bonais: $400
+// Palito de helado de agua: $1.000 
+// Palito de helado de crema: $2.200
+// Bombón helado con arequipe: $1.500
+// Bombón helado con chispas de chocolate: $2.500
+// Bombón helado con fresas: $2.800
+// Medio litro de helado: $ 2.000
 
-let frito = confirm("Desea el huevo frito?\n\t\tSelecciona Aceptar si desea el huevo frito\n\t\tSelecciona Cancelar si desea el huevo cocido");
-let sal = confirm("Desea el huevo con sal?\n\t\tSelecciona Aceptar si desea sal\n\t\tSelecciona Cancelar si no desea sal");
-if(frito == true && sal == true)
-{
-    alert("El huevo pedido fue frito, con sal y servido en el plato");
+// El programa tiene que pedir el ingreso de su dinero y el nombre de cliente
+// Si hay más de un helado con el mismo precio o por de bajo de precio del usuario mostrar las opciones disponibles
+// El programa también tiene que poder dar los vueltos de su compra con su factura
+var dinero = [];
+var nombre = [];
+var precios = [["Bonais",400],["Palito de helado de agua",1000],["Bombón helado con arequipe",1500],["Medio litro de helado",2000],["Palito de helado de crema",2200],["Bombón helado con chispas de chocolate",2500],["Bombón helado con fresas",2800]];
+for (let i = 1; i < 4; i++){
+dinero.push( Number ( prompt (`Ingrese el dinero del usuario ${i}:`)));
+nombre.push( prompt (`Ingrese el nombre del usuario ${i}:`));}
+var helado = [];
+for(i = 0; i < dinero.length; i++){
+    let preciomaximo = 0;
+    console.log(` El usuario ${nombre[i]} tiene las opciones : \n `);
+    for(let a = 0; a <precios.length; a++){
+         if(dinero[i]>=precios[a][1]){
+            preciomaximo= a;
+            console.log(precios[a]);
+         }        
 }
-else
-{
-    if(frito == true && sal == false)
-    {
-        alert("El huevo pedido fue frito, sin sal y servido en el plato")
-    }
-    else
-    {
-        if(frito == false && sal == true)
-        {
-        alert("El huevo pedido fue cocido, con sal y servido en el plato");
-        }
-        else
-        {
-            if(frito == false && sal == false)
-            {
-            alert("El huevo pedido fue cocidoo, sin sal y servido en el plato")
-            }
-        }
-    }
+helado.push({Nombre: nombre[i], Helado: precios[preciomaximo][0], Precio: precios[preciomaximo][1], Vueltos: dinero[i]-precios[preciomaximo][1]});
 }
+console.log(`Lo que compraron los usuarios fue los siguiente: `)
+console.table(helado);
